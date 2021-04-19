79cb0843140187b268f2e477933d426541fe4989
shitij <21.shitijagrawal@gmail.com>
2021-04-19 15:01:30 +0530

    added new category

--- redis-sharding/redis-tls-container/startup.sh
+++ redis-sharding/redis-tls-container/startup.sh
@@ -8,7 +8,7 @@ docker-compose up -d --build
 wait
 sleep 30
 
-docker exec redis_tls_shopview bash -c "redis-server /usr/local/etc/redis/redis.conf"
+docker exec -d redis_tls_shopview bash -c "redis-server /usr/local/etc/redis/redis.conf"
 
 sudo chmod 555 -R certs/
 
